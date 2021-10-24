185d07e5856f7b4e8f9e2a9f9b1fa1b68959a7db
Nazarii <nazariikubik@gmail.com>
2021-10-24 21:43:51 +0300

    Update 0.0.1

--- main.js
+++ main.js
@@ -352,3 +352,18 @@ $(document).ready(function () {
     }, 2000);
   });
 });
+
+let btn = $("#button");
+
+$(window).scroll(function () {
+  if ($(window).scrollTop() > 500) {
+    btn.addClass("show");
+  } else {
+    btn.removeClass("show");
+  }
+});
+
+btn.on("click", function (e) {
+  e.preventDefault();
+  $("html, body").animate({ scrollTop: 0 }, 1000);
+});
